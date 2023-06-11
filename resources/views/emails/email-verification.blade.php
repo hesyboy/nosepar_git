a3cbd21269023be792e15db3a3c994716fb88117
hesyboy <hesamahmoodi@gmail.com>
2023-06-12 01:09:52 +0330

    email template

--- resources/views/emails/email-verification.blade.php
+++ resources/views/emails/email-verification.blade.php
@@ -1,4 +1,27 @@
-    <div>
-        Your code is : {{ $user }}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>No Separ | Email Verification</title>
+</head>
+<body>
+    <div >
+        <h2>
+            No Separ | Email Verification
+        </h2>
     </div>
+    <div style="background-color: royalblue;color:white">
+        <h3>
+            <span>
+                Your Code is :
+            </span>
+            <span>
+                {{ $user }}
+            </span>
+        </h3>
+    </div>
+</body>
+</html>
 
